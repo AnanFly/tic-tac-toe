ba44976dbcd452e588acfda240bb7936cca24d17
jiangnan <2950857517@qq.com>
2024-03-18 12:22:06 +0800

    #FIX: [棋局] 修复eslint不生效问题

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,5 @@
 module.exports = {
   printWidth: 80,
   tabWidth: 2,
-  useTabs: false,
-  singleQuote: true, // 表示使用单引号
-  semi: false, //  表示不使用分号
-  trailingComma: 'none',
-  bracketSpacing: true
-}
+  endOfLine: "auto",
+};
